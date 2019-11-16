342de915eea542b727f95d7587215f7af9c68b39
ChenLin12138 <scorpion_chenlin@163.com>
2019-11-16 22:22:01 +0800

    修改zipkin的配置

--- src/main/java/com/chenlin/zipkin/ZipkinServiceApplication.java
+++ src/main/java/com/chenlin/zipkin/ZipkinServiceApplication.java
@@ -3,7 +3,11 @@ package com.chenlin.zipkin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import zipkin2.server.internal.EnableZipkinServer;
+
 @SpringBootApplication
+//允许快速启动zipkin作为springboot的项目
+@EnableZipkinServer
 public class ZipkinServiceApplication {
 
 	public static void main(String[] args) {
