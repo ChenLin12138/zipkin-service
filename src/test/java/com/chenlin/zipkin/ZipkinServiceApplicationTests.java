4a1810425f8ac95b50eda23fd8a6610b08a112e6
ChenLin12138 <scorpion_chenlin@163.com>
2019-11-16 20:08:20 +0800

    Project init

---
+++ src/test/java/com/chenlin/zipkin/ZipkinServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenlin.zipkin;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
